d3df0a969646922ad2a46a92b1152c1f4349d776
Pius Ezekiel <pius@Devil-PC>
2023-10-04 11:54:53 +0200

    major commit v3

--- models/amenity.py
+++ models/amenity.py
@@ -1,29 +1,15 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
-""" Amenity Module for HBNB project """
+""" State Module for HBNB project """
+import os
 
-=======
-"""This is the amenity class"""
->>>>>>> parent of 06df725 (models)
 from models.base_model import BaseModel, Base
-from sqlalchemy.orm import relationship
 from sqlalchemy import Column, String
-from models.place import place_amenity
+from sqlalchemy.orm import relationship
 
 
 class Amenity(BaseModel, Base):
-<<<<<<< HEAD
-    """ Amenity class """
+    """Amenities of a place"""
     __tablename__ = 'amenities'
-
     name = Column(String(128), nullable=False)
-    place_amenities = relationship("Place", secondary='place_amenity', viewonly=False)
-=======
-    """This is the class for Amenity
-    Attributes:
-        name: input name
-    """
-    __tablename__ = "amenities"
-    name = Column(String(128), nullable=False)
-    place_amenities = relationship("Place", secondary=place_amenity)
->>>>>>> parent of 06df725 (models)
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        place_amenities = relationship('Place', secondary="place_amenity")
--- models/user.py
+++ models/user.py
@@ -1,42 +1,17 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
-""" User Module for HBNB project """
-
-=======
-"""This is the user class"""
-from sqlalchemy.ext.declarative import declarative_base
->>>>>>> parent of 06df725 (models)
+"""This module defines a class User"""
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from models.place import Place
-from models.review import Review
 
 
 class User(BaseModel, Base):
-<<<<<<< HEAD
-    """ User class """
+    """This class defines a user by various attributes"""
     __tablename__ = 'users'
 
-=======
-    """This is the class for user
-    Attributes:
-        email: email address
-        password: password for you login
-        first_name: first name
-        last_name: last name
-    """
-    __tablename__ = "users"
->>>>>>> parent of 06df725 (models)
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128))
     last_name = Column(String(128))
-<<<<<<< HEAD
-    reviews = relationship("Review", backref="user", cascade="all, delete")
-=======
-    places = relationship("Place", cascade='all, delete, delete-orphan',
-                          backref="user")
-    reviews = relationship("Review", cascade='all, delete, delete-orphan',
-                           backref="user")
->>>>>>> parent of 06df725 (models)
+    places = relationship("Place", backref="user", cascade="delete")
+    reviews = relationship("Review", backref="user", cascade="delete")
